7edf249845ab745ce09ce4d80a011bfa9476e932
Salith <maxcabourg@hotmail.fr>
2015-12-18 17:33:57 +0100

    Creation de la liaison entre ServeurFact et ClientFact

--- Projet_Reseau/src/Client/ClientFact.java
+++ Projet_Reseau/src/Client/ClientFact.java
@@ -1,5 +1,61 @@
 package Client;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+/*
+ * ClientFact est une classe representant un client capable de se connecter à un serveur effectuant le calcul d'une factorielle.
+ * @author Max Cabourg
+ */
 public class ClientFact {
+	
+	/*
+	 * Socket du client indiquant sur qui il va envoyer la demande
+	 */
+	private static Socket socket;
+	/*
+	 * Adresse du serveur destinataire
+	 */
+	private static InetAddress adresse;
+	/*
+	 * Adresse du port
+	 */
+	private static int port;
+	
+	/*
+	 * Constructeur de la classe ClientFact
+	 * @param argv
+	 * Parametres de la ligne de commande : l'ip du serveur puis son port
+	 */
+	public ClientFact(String[] argv){
+		try {
+			adresse = InetAddress.getByName(argv[0]); //Recupere l'adresse ip
+			port = Integer.parseInt(argv[1]); //Recupere le port
+			socket = new Socket(adresse, port);	
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] argv){	
+		ClientFact client = new ClientFact(argv);
+		try {
+			PrintStream output = new PrintStream(socket.getOutputStream());
+			Scanner sc = new Scanner(System.in);
+		    String msg;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+			
+	}
 
 }
--- Projet_Reseau/src/Serveurs/ServeurFact.java
+++ Projet_Reseau/src/Serveurs/ServeurFact.java
@@ -1,5 +1,47 @@
 package Serveurs;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+ * Classe representant un serveur capable de calculer une factorielle
+ * @author Max Cabourg
+ */
 public class ServeurFact {
+	
+	/*
+	 * Port sur lequel ecoute le serveur
+	 */
+	private static int port;
+	/*
+	 * Le cache contiendra les calculs déjà fait précédemment durant l'execution du serveur
+	 */
+	private static ArrayList cache;
+	/*
+	 * Le serveur en lui meme
+	 */
+	private static ServerSocket socket;
+	
+	public static void main(String argv[]){	
+		try {
+			port = Integer.parseInt(argv[1]);
+			cache = new ArrayList();
+			socket = new ServerSocket(port);
+		    Socket s = socket.accept(); //Accepte la connexion TCP
+			
+			Scanner sc = new Scanner(s.getInputStream());
+		    while (true) {
+		      String msg = sc.nextLine();
+		      System.out.println("Received: " + msg);
+		    }//while
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
