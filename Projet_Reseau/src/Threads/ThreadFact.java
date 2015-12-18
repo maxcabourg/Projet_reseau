9309e0a488900be14aef74b38f7e2a43d3b04301
Salith <maxcabourg@hotmail.fr>
2015-12-18 20:25:24 +0100

    Commentaires ajoutés.

--- Projet_Reseau/src/Threads/ThreadFact.java
+++ Projet_Reseau/src/Threads/ThreadFact.java
@@ -2,6 +2,7 @@ package Threads;
 
 /*
  * Thread permettant de calculer la factorielle d'un nombre
+ * @author Max Cabourg
  */
 public class ThreadFact extends Thread{
 
@@ -15,15 +16,27 @@ public class ThreadFact extends Thread{
 	 */
 	private int resultat;
 	
+	/*
+	 * Constructeur de ThreadFact
+	 * @param _entree : Valeur dont on souhaite connaitre la factorielle
+	 */
 	public ThreadFact(int _entree){
 		super();
 		entree = _entree;
 	}
 	
+	/*
+	 * Redefinition de la methode run pour qu'elle calcule la factorielle
+	 */
+	@Override
 	public void run(){
 		
 	}
 	
+	/*
+	 * getter du resultat
+	 * @return Le resultat de la factorielle
+	 */
 	public int getResultat(){
 		return resultat;
 	}
