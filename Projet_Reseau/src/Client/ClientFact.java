25114a227c1593ae00b5e67fb000a5f7401610de
Salith <maxcabourg@hotmail.fr>
2016-01-22 14:30:18 +0100

    JavaDoc de Factorielle

--- Projet_Reseau/src/Client/ClientFact.java
+++ Projet_Reseau/src/Client/ClientFact.java
@@ -6,20 +6,37 @@ import java.io.PrintStream;
 import java.net.*;
 import java.util.Scanner;
 
+/**
+ * Classe représentant un client envoyant un nombre a un serveur et recuperant la factorielle de celui ci
+ * @author max
+ *
+ */
 public class ClientFact extends Thread{
 
-
+	/**
+	 * Adresse a laquelle va se connecter le serveur
+	 */
 	private InetAddress adresse;
+	/**
+	 * Socket associé au port et a l'adresse
+	 */
 	private Socket socket;
+	/**
+	 * Nombre dont on souhaite connaitre la factorielle
+	 */
 	private String param;
+	/**
+	 * Port sur lequel le client va envoyer le nombre au serveur
+	 */
 	private int port;
+	/**
+	 * Valeur de retour
+	 */
 	private String retour;
 
-	/*Client de la fonction de calcul Factorielle 
-	 *	Paramètres:
-	 * 1, le paramètre de la fonction Factorielle
-	 * 2, le port du serveur
-	 * 3, l'adresse du serveur
+	/**
+	 * Fonction main du client
+	 * @param args Le 1er argument correspond au nombre pour la factorielle, le 2eme au port et le 3eme a l'adresse
 	 */
 	public static void main(String[] args) {
 		//Création du client
@@ -33,6 +50,13 @@ public class ClientFact extends Thread{
 		}
 	}
 
+	/**
+	 * Constructeur principal du Client
+	 * @param param Nombre dont on souhaite connaitre la factorielle
+	 * @param port Port sur lequel on va envoyer le nombre
+	 * @param adresse Adresse du serveur
+	 * @throws IOException En cas d'erreur de connexion
+	 */
 	public ClientFact(String param,int port, String adresse) throws IOException{
 		super();
 		this.port = port;
@@ -42,13 +66,15 @@ public class ClientFact extends Thread{
 
 	}
 
+	/**
+	 * Fonction faisant marcher le client. Gere les flux d'entree sortie principalement.
+	 */
 	public void run(){
 		try {
-			//Connection au Serveur
-			//Envoie du paramètre
+			//On envoie au nombre
 			PrintStream output = new PrintStream(socket.getOutputStream());
 			output.print(this.param+"\n");
-			//Attente de la réponse du serveur
+			//Listener
 			InputStream input = socket.getInputStream();
 			Scanner sc = new Scanner(input);
 			String msg;
@@ -62,7 +88,11 @@ public class ClientFact extends Thread{
 		}
 
 	}
-
+	
+	/**
+	 * Getter de retour
+	 * @return Renvoie la valeur de retour
+	 */
 	public String getRetour() {
 		return retour;
 	}
--- Projet_Reseau/src/Serveurs/ServeurFact.java
+++ Projet_Reseau/src/Serveurs/ServeurFact.java
@@ -6,25 +6,71 @@ import java.util.*;
 
 import Client.ClientFact;
 
+/**
+ * Classe representant un serveur capable de calculer la factorielle d'un nombre.
+ * Le serveur se charge de creer des sous clients qui vont a leur tour se connecter au serveur
+ * @author max
+ *
+ */
 public class ServeurFact extends Thread{
 
+	
+
+	/**
+	 * Port sur lequel on écoute
+	 */
+	private int port;
+	/**
+	 * Cache du serveur qui va aller fouiller si on a pas deja calculé la factorielle du nombre pour gagner du temps
+	 */
+	private HashMap<Integer,Integer> cache;
+	
+	/**
+	 * Fonction main du serveur. Le programme part de là.
+	 * @param args 1er argument : Le port sur lequel écouter
+	 */
 	public static void main(String[] args) {
 		ServeurFact server = new ServeurFact(Integer.parseInt(args[0]));
 		server.start();
 	}
 
-	private int port;
-	private HashMap<Integer,Integer> cache;
-
+	/**
+	 * Classe interne qui va se charger de gerer un client qui se connecte au serveur.
+	 * Elle stockera notemment le flux d'entree et de sortie de ce client
+	 * @author max
+	 *
+	 */
 	private class ClientThread extends Thread{
-
+		
+		/**
+		 * Port du client
+		 */
 		private int port;
+		/**
+		 * Socket du client
+		 */
 		private Socket socket;
+		/**
+		 * Nombre associé a ce client
+		 */
 		private int param;
+		/**
+		 * Flux de sortie du client
+		 */
 		private PrintStream sortie;
+		/**
+		 * Cache du serveur qui est en fait partagé par tous les clients
+		 */
 		private HashMap<Integer,Integer> cache;
 
 
+		/**
+		 * Constructeur de clientThread
+		 * @param socket Socket que l'on va associer a ce thread
+		 * @param cache Cache du serveur partagé entre chaque clientThread
+		 * @param port Port du client
+		 * @throws Exception La connextion peut echouer
+		 */
 		public ClientThread(Socket socket,HashMap<Integer,Integer> cache,int port) throws Exception{
 			super();
 			this.socket = socket;
@@ -36,24 +82,24 @@ public class ServeurFact extends Thread{
 
 		}
 
-		synchronized public void run(){
+		/**
+		 * Fonction déclenchant le traitement du client.
+		 * Elle s'occupera notamment de creer les sous clients.
+		 */
+		public void run(){
 			try{	
-				//Traitement de la demande
-				if(this.cache.containsKey(param)){//On a deja effectue le calcul
+				if(this.cache.containsKey(param))//On a deja effectue le calcul
 					System.out.println("Factorielle de "+param+" vaut : "+this.cache.get(param));
-				}
 				else
 				{
-					if(param == 0)//Condition d'arrêt de récursivité
+					if(param == 0)
 						this.cache.put(0,1);	
 					else
 					{
-						//On crée un nouveau Client
 						ClientFact c = new ClientFact(Integer.toString(param-1),this.port,"localhost");
 						c.start();
 						while(c.getRetour()==null);
-						//On attends que le client ait sa réponse
-						this.cache.put(param,Integer.parseInt(c.getRetour())*param);//On remplit le cache			
+						this.cache.put(param,param*Integer.parseInt(c.getRetour()));		
 					}
 				}
 				//Envoie au client et fermeture de la connexion
@@ -66,20 +112,22 @@ public class ServeurFact extends Thread{
 	}
 
 
-	/*Server de la fonction de calcul Factorielle écoutant sur le port 50000
-	 * Paramètres:
-	 * 1, port sur lequel le serveur écoute
+	/**
+	 * Constructeur du serveur
+	 * @param port Port sur lequel il va ecouter
 	 */
-
 	public ServeurFact(int port){
 		super();
 		this.port = port;
-		this.cache = new HashMap<Integer,Integer>();
+		cache = new HashMap<Integer,Integer>();
 
 	}
 
 
-	@SuppressWarnings("resource")
+	/**
+	 * Fonction faisant tourner le serveur.
+	 * Il s'attache a creer un client Thread a chaque fois qu'un client se connecte
+	 */
 	public void run(){
 		try{
 			ServerSocket serverSocket = new ServerSocket(port);
